0c78182d5474cd8752f98cbf57ee4f96f763066e
Dhruv Dani <dhruvbdani@gmail.com>
2022-02-28 16:41:52 +0000

    Update main.js

--- Week 4/Exercise 4/main.js
+++ Week 4/Exercise 4/main.js
@@ -4,7 +4,7 @@ function onLoad()
     var heading1 = document.getElementById("heading1");
     var paragraph1 = document.getElementById("paragraph1");
     
-    heading1.innerHTML = "Hello, my name is <i>Dhruv</i>.";
+    heading1.innerHTML = "Hello, my name is <u><i>Dhruv</i></u>.";
     paragraph1.firstChild.nodeValue = "This is the newly updated text.";
 }
 
